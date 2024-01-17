b610078fb6ca9f9edc11079df82d9f8bbcd58369
rfranklin2 <rfranklin2@CIC5770-B3DVFX3>
2024-01-17 13:30:29 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darthgoose
+{
+    internal class Program
+    {
+        public static void BUTTON()
+        {
+            Window1 window = new Window1();
+            window.Show();
+        }
+    }
+}
